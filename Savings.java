65f4eba238dd60f5798caec5560e3960ab8e731c
Salma Magdy <salmamagdyelsayed@gmail.com>
2026-07-19 18:30:20 +0300

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -13,14 +13,24 @@ public class Savings extends Account {
     void withdraw(double withdraw)
     {
         System.out.printf("%s tries to withdraw %.2f \n" , super.getName() , withdraw);
-        if (super.getBalance() - withdraw >= minimumBalance)
+        if (super.getBalance() - withdraw >= minimumBalance && withdraw > 0)
         {
             super.setBalance(super.getBalance() - withdraw);
             System.out.printf("[SUCCESS] %s withdrew %.2f, Current Balance: %.2f \n", super.getName(), withdraw, super.getBalance());
+
+            Logger.getInstance().produce(
+                    String.format("%s withdrew %.2f (savings), new balance %.2f", getName(), withdraw, getBalance()),
+                    Logger.log.INFO
+            );
         }
         else
         {
             System.out.printf("[REJECTED] Transaction denied for %s. Minimum balance limit (%.0f) breached. Current Balance: %.2f \n", getName(), minimumBalance ,getBalance());
+
+            Logger.getInstance().produce(
+                    String.format("%s REJECTED withdrawal of %.2f (savings, min balance breach)", getName(), withdraw),
+                    Logger.log.WARNING
+            );
         }
     }
 
