e1dac07101c54759bb9a974ee26b62fb4a518dfe
Salma Magdy <salmamagdyelsayed@gmail.com>
2026-07-19 18:30:45 +0300

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -12,15 +12,25 @@ public class Checking extends Account {
     void withdraw(double withdraw)
     {
         System.out.printf("%s tries to withdraw %.2f \n" , getName() ,withdraw);
-        if (withdraw + operationFee <= getBalance())
+        if (withdraw + operationFee <= getBalance() && withdraw > 0)
         {
             setBalance(getBalance() - withdraw - operationFee);
             System.out.printf("[SUCCESS] %s withdrew %.2f , Current Balance: %.2f \n", getName() , withdraw , getBalance());
+
+            Logger.getInstance().produce(
+                    String.format("%s withdrew %.2f (checking), new balance %.2f", getName(), withdraw, getBalance()),
+                    Logger.log.INFO
+            );
         }
         else
         {
             System.out.printf("[REJECTED] Transaction denied for %s. Insufficient balance \n", getName());
             System.out.printf("Current balance is: %.2f \n" , getBalance());
+
+            Logger.getInstance().produce(
+                    String.format("%s REJECTED withdrawal of %.2f (checking, insufficient balance)", getName(), withdraw),
+                    Logger.log.WARNING
+            );
         }
 
     }
